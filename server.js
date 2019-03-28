90f344c2b774520d64fbbe2fb04a30f8bf31fffe
John Lund <johnrlund0@gmail.com>
2019-03-28 16:32:21 -0600

    update footer and port

--- server.js
+++ server.js
@@ -108,4 +108,4 @@ app.put('/api/posts/:id', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3030, () => console.log('Server listening on port 3000!'));
